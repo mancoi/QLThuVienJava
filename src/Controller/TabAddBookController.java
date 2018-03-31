488f622db6d857ba94a6656f230531a8adb8c47c
mancoi <caoman96@gmail.com>
2018-03-31 18:33:49 +0700

    Add action for the buttons, add book button now working

--- src/Controller/TabAddBookController.java
+++ src/Controller/TabAddBookController.java
@@ -5,9 +5,23 @@
  */
 package Controller;
 
+import Data.ComboBoxData;
+import Data.Database;
+import QLThuVien.Book;
+import QLThuVien.QueryHelper;
+import QLThuVien.Utils;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -16,12 +30,66 @@ import javafx.fxml.Initializable;
  */
 public class TabAddBookController implements Initializable {
 
+    @FXML
+    private TextField txtBookTitle;
+    @FXML
+    private TextField txtAuthor;
+    @FXML
+    private ComboBox cbGenre;
+    @FXML
+    private ComboBox cbPublisher;
+    @FXML
+    private TextField txtYearPublished;
+    @FXML
+    private Label lbAddBookStatus;
+    @FXML
+    private Button butAddBook;
+    @FXML
+    private Label lbUpdateBookId;
+    @FXML
+    private Button butUpdateBook;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        cbGenre.getItems().addAll(ComboBoxData.populateCmbBoxTheLoai());
+        cbPublisher.getItems().addAll(ComboBoxData.populateCmbBoxNxb());
+    }
+
+    @FXML
+    protected void butAddBkAction(ActionEvent event) {
+
+        if (txtBookTitle.getText().isEmpty()
+                || txtAuthor.getText().isEmpty()
+                || cbPublisher.getEditor().getText().isEmpty()
+                || txtYearPublished.getText().isEmpty()) {
+            Utils.showAlertOptional(
+                    "Không được để trống bất kỳ trường nào.", "Thiếu thông tin", Alert.AlertType.INFORMATION);
+        } else {
+            Book bk = new Book();
+            bk.setTitle(txtBookTitle.getText());
+            bk.setAuthor(txtAuthor.getText());
+            if (cbGenre.getSelectionModel().isEmpty()) {
+                bk.setGenre("");
+            } else {
+                bk.setGenre(cbGenre.getSelectionModel().getSelectedItem().toString());
+            }
+            bk.setPublisher(cbPublisher.getEditor().getText());
+            bk.setYearPublish(txtYearPublished.getText());
+            bk.addBook(lbAddBookStatus);
+            Database.populateTable(QueryHelper.selectAllBooks());
+        }
+    }
+
+    @FXML
+    protected void butUpdateBkAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    protected void butDelBkAaction(ActionEvent event) {
+
+    }
 }
